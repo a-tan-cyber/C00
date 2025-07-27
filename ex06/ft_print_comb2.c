8dfdf799e84038c4a58e0f62d2eed54556175ed8
atan94 <amostan94@gmail.com>
2025-07-27 18:40:51 +0800

    Complete ex06.

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 17:18:45 by amtan             #+#    #+#             */
+/*   Updated: 2025/07/27 18:21:27 by amtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	first_number;
+	int	second_number;
+
+	first_number = 0;
+	while (first_number <= 98)
+	{
+		second_number = first_number + 1;
+		while (second_number <= 99)
+		{
+			ft_putchar(first_number / 10 + '0');
+			ft_putchar(first_number % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(second_number / 10 + '0');
+			ft_putchar(second_number % 10 + '0');
+			if (first_number != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			second_number++;
+		}
+		first_number++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+}
